1b3abac60722d237565e04a341c636b04ff3b032
Chris Seaton <chris.seaton@shopify.com>
2021-01-26 23:50:12 +0000

    Update

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ sudo apt-get update
 sudo apt-get install -y autoconf bison build-essential clang git libssl-dev ruby
 
 git clone https://github.com/angussidney/ruby
-cp /var/mmtk/libmmtk_ruby.so ruby
+cp /var/mmtk/libmmtk_ruby.so-release ruby/libmmtk_ruby.so
 pushd ruby
 autoconf
 export LD_LIBRARY_PATH=$PWD
 CFLAGS="-O0 -ggdb3 -DUSE_THIRD_PARTY_HEAP -DUSE_TRANSIENT_HEAP=0" ./configure --prefix=$PWD/build
-make install
+RUST_BACKTRACE=true make install
 PATH=$PWD/build/bin:$PATH
 popd
 
